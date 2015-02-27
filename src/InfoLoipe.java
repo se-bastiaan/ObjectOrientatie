5acfa25c4220da3c3d38afe428478dc2895d9c4e
Sébastiaan <se_bastiaan@outlook.com>
2015-02-27 09:43:55 +0100

    Assignment 4: start

---
+++ src/InfoLoipe.java
@@ -0,0 +1,14 @@
+/**
+ * @author Sébastiaan Versteeg / s4459636
+ */
+public interface InfoLoipe {
+    public int getWidth();
+
+    public int getHeight();
+
+    public Fragment getFragment(int x, int y);
+
+    public Punt start();
+
+    public Punt stap();
+}
